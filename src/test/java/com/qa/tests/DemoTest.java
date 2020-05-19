d6ed6b66bdb037f93b6aaa6a486578bc8ed3a629
FatimaCornell20 <64547929+FatimaCornell20@users.noreply.github.com>
2020-05-19 15:25:01 -0400

    this is the change for jen git kinkage test

---
+++ src/test/java/com/qa/tests/DemoTest.java
@@ -0,0 +1,41 @@
+package com.qa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	@Test
+	public void sum() {
+		System.out.println("this is SUM method");
+		int a=10;
+		int b=20; 
+		Assert.assertEquals(30, a+b);
+		
+	}
+	@Test
+	public void sub() {
+		System.out.println("this is SUB method");
+		int a=10;
+		int b=20; 
+		Assert.assertEquals(10, b-a);
+		
+	}
+	@Test
+	public void div() {
+		System.out.println("this is DIV method");
+		int a=10;
+		int b=20; 
+		Assert.assertEquals(2, b/a);
+		
+	}
+	@Test
+	public void multi() {
+		System.out.println("this is MULTI method");
+		int a=10;
+		int b=20; 
+		Assert.assertEquals(200, a*b);
+		
+	}
+
+}
